91adf5076ae49fa7fdd74f66d9141a1e6e7e953f
AKHIL-S123 <amwonders1@gmail.com>
2023-09-30 12:00:53 +0530

    fourth

--- project/app/templatetags/custom_filters.py
+++ project/app/templatetags/custom_filters.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter(name='has_permission')
 def has_permission(user, permission):
     return user.has_perm(permission)
+
+@register.filter(name='has_any_permission')
+def has_any_permission(user, permissions):
+    permissions_list = permissions.split(',')
+    for permission in permissions_list:
+        if user.has_perm(permission.strip()):
+            return True
+    return False
